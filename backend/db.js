c73c529a66c5aad02a08f2b2b74cfb6cb1cf51f4
FCJ <iit2020218@iiita.ac.in>
2022-05-09 13:50:36 +0530

    Adding Project to the MongoDb Server

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose');
-const mongoUrl = "mongodb://localhost:27017/Proma?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
+require("dotenv").config();
+const mongoUrl = `mongodb+srv://FCJ1234:kY7DydCrlFfo7OaI@cluster0.zwfl2.mongodb.net/proma?retryWrites=true&w=majority`
+const Key = process.env.REACT_APP_USER_PASS;
 
 
 
 const connectToDatabase = () =>{
-    mongoose.connect(mongoUrl,() =>{
+    mongoose.connect(mongoUrl,{
+        useNewUrlParser : true,
+        useUnifiedTopology : true
+    },() =>{
         console.log("database connected successfully");
+        //console.log(mongoUrl)
+        //console.log(Key)
     })
 }
 
