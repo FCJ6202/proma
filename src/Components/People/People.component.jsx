6aa72cd0fcb653810d5a4a68c758db7ec72c1dd0
mitrya <cnexus05@gmail.com>
2022-05-10 16:41:06 +0530

    do people page UI

--- src/Components/People/People.component.jsx
+++ src/Components/People/People.component.jsx
@@ -1,8 +1,111 @@
 import './People.styles.css'
-
+import {FaUserPlus} from "react-icons/fa"
+import image from "../../images/profile_pic.png"
+    const Professors=[
+        {
+            name:"OP Vyas"
+        },
+        {
+            name:"Bagesh Kumar"
+        }
+    ]
+    const Assistants=[
+     {
+         name:"Nand Yadav"
+     },
+     {
+         name:"Aditya Singh"
+     }  
+    ]
+    const Students=[
+        {
+            name:"Aman Utkarsh"
+        },
+        {
+            name:"Jitu Rajak"
+        }  
+       ]
 const People = () => (
 
-    <div>under construction</div>
+    <>
+        <div className='people-page'>
+            <div className='title-head'>
+                <div className='title'>Professors</div>
+        
+                <button className='invite'>
+                    <FaUserPlus className='icon'/>
+                </button>
+            </div>
+            <hr/>
+            {
+                Professors?.map((Professor)=>
+                {
+                    return (
+                        <>
+                            <div className='item'>
+                                <div className="image">
+                                    <img src={image} alt="Profile photo" />                            
+                                </div>
+
+                                <div className='name'>{Professor.name}</div>        
+                            </div>
+                            <hr className='item-line'/>
+                        </>
+                    )
+                })
+            }
+            <div className='title-head'>
+                <div className='title'>Assistants</div>
+                <button className='invite'>
+                    <FaUserPlus className='icon'/>
+                </button>
+            </div>
+            <hr/>
+            {
+                Assistants?.map((Professor)=>
+                {
+                    return (
+                        <>
+                            <div className='item'>
+                                <div className="image">
+                                    <img src={image} alt="Profile photo" />                            
+                                </div>
+
+                                <div className='name'>{Professor.name}</div>        
+                            </div>
+                            <hr className='item-line'/>
+                        </>
+                    )
+                })
+            }
+
+            <div className='title-head'>
+                <div className='title'>Students</div>
+                <button className='invite'>
+                    <FaUserPlus className='icon'/>
+                </button>
+            </div>
+            <hr/>
+            {
+                Students?.map((Professor)=>
+                {
+                    return (
+                        <>
+                            <div className='item'>
+                                <div className="image">
+                                    <img src={image} alt="Profile photo" />                            
+                                </div>
+
+                                <div className='name'>{Professor.name}</div>        
+                            </div>
+                            <hr className='item-line'/>
+                        </>
+                    )
+                })
+            }
+
+        </div>
+    </>
 )
 
 export default People;
